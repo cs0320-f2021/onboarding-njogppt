052677f409128e55218f4ffa123b72736af38e06
Futen Wang <futen_wang@brown.edugit config --global user.email futen.wang@gmail.com>
2021-09-15 23:18:03 -0400

    save

--- src/test/java/edu/brown/cs/student/main/MathBotTest.java
+++ src/test/java/edu/brown/cs/student/main/MathBotTest.java
@@ -28,4 +28,45 @@ public class MathBotTest {
   }
 
   // TODO: add more unit tests of your own
+  @Test
+  public void testAdditionWithBothNegative() {
+    MathBot matherator9000 = new MathBot();
+    double output = matherator9000.add(-5, -3.4);
+    assertEquals(-8.4, output, 0.01);
+  }
+
+  @Test
+  public void testBothDecimals() {
+    MathBot matherator9001 = new MathBot();
+    double output = matherator9001.add(4.5, -2.1);
+    assertEquals(2.4, output, 0.01);
+  }
+
+  @Test
+  public void testSubtraction2() {
+    MathBot matherator9002 = new MathBot();
+    double output = matherator9002.subtract(18.0, 7.0);
+    assertEquals(11.0, output, 0.01);
+  }
+
+  @Test
+  public void testAdditionWithTensPlace() {
+    MathBot matherator9000 = new MathBot();
+    double output = matherator9000.add(10.54, 3);
+    assertEquals(13.54, output, 0.01);
+  }
+
+  @Test
+  public void testLargerNumbers3() {
+    MathBot matherator9001 = new MathBot();
+    double output = matherator9001.add(100, 200303);
+    assertEquals(200403, output, 0.01);
+  }
+
+  @Test
+  public void testSubtractionWithNegatives() {
+    MathBot matherator9002 = new MathBot();
+    double output = matherator9002.subtract(-18, -17);
+    assertEquals(-1, output, 0.01);
+  }
 }
